1725609501d5b0fcb73faa4440c223bfc07859d4
Luis Quiroz <luisqpra@gmail.com>
2023-11-20 09:51:26 -0500

    feature(test_1) - Creacion de los primeros test

---
+++ Pytest_topics/test_module01.py
@@ -0,0 +1,15 @@
+def test_a1():
+    assert 5 + 5 == 10
+    # No es recomendado tener varios assert en  una misma funcion test
+    assert 5 - 5 == 0  # esto es solo pro practiva
+    assert 5 * 5 == 25
+    assert 5 / 5 == 1
+
+
+def test_a2():
+    assert 9/5 == 1.5, "failed test intencionalmente"
+
+
+# test no.3
+def test_a3():
+    assert 9//5 == 1
